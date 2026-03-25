95eccf6dc999080a423c1bc14c05a7c78781e697
Gurdip Sira <87333788+GurdipS5@users.noreply.github.com>
2026-03-25 17:50:34 +0000

    Update baseline.ps1

--- buildkite-agents/python/baseline.ps1
+++ buildkite-agents/python/baseline.ps1
@@ -1,1 +1,8 @@
 
+pip install git-cliff
+scoop install lefthook
+choco install ggshield
+pip install ruff
+pip install pysonar
+New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\FileSystem" -Name "LongPathsEnabled" -Value 1 -PropertyType DWORD -Force
+pip install bandit
