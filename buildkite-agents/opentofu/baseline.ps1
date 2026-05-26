31bd8acf498bd164fa614c56b04f2a44f3bc1ee1
Gurdip Sira <87333788+GurdipS5@users.noreply.github.com>
2026-05-26 01:17:58 +0100

    Create baseline.ps1

---
+++ buildkite-agents/opentofu/baseline.ps1
@@ -0,0 +1,10 @@
+choco install opentofu -y
+choco install golang -y
+choco install terragrunt -y
+choco install tflint -y
+choco install trivy -y
+choco install terraform-docs -y
+choco install git -y
+choco install checkov -y
+choco install tfsec -y
+winget install git-cliff.
